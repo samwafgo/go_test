a3dea98707c786bbe33e9098255ac02e1dad6420
samwaf <samwafgo@gmail.com>
2024-09-04 09:44:16 +0800

    2

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go_test/global"
+)
 
 func main() {
 	var tip = "hello this is go build test"
 	fmt.Println(tip)
+	fmt.Println("GWAF_RELEASE:" + global.GWAF_RELEASE)
+	fmt.Println("GWAF_RELEASE_VERSION_NAME:" + global.GWAF_RELEASE_VERSION_NAME)
+	fmt.Println("GWAF_RELEASE_VERSION:" + global.GWAF_RELEASE_VERSION)
 }
