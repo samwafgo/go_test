087478c4f9b4f00a39ea2916cb3b23e958c66897
samwafgo <138433335+samwafgo@users.noreply.github.com>
2024-09-04 09:30:40 +0800

    Create global.go

---
+++ global/global.go
@@ -0,0 +1,9 @@
+package global
+
+
+var (
+
+	GWAF_RELEASE              string    = "false"     // 当前是否为发行版
+	GWAF_RELEASE_VERSION_NAME string    = "1.0"       // 发行版的版本号名称
+	GWAF_RELEASE_VERSION      string    = "v1.0.0"    // 发行版的版本号
+  )
